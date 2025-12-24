32c136ab21f83ca67ba904bb77c2659ddacab6ee
Ariel Silahian <silahian@hotmail.com>
2025-12-23 23:22:18 -0500

    Refactor HelperOrderBook for thread-safe subscriber mgmt
    Replaced List with ImmutableArray for subscriber storage and
    used ImmutableInterlocked for atomic updates. Removed manual
    locking in Subscribe, Unsubscribe, and Reset methods. Improved
    thread safety and performance by leveraging immutable collections
    and streamlined exception handling in DispatchToSubscribers.

--- VisualHFT.Commons/Helpers/HelperOrderBook.cs
+++ VisualHFT.Commons/Helpers/HelperOrderBook.cs
@@ -1,4 +1,5 @@
-﻿using VisualHFT.Model;
+﻿using System.Collections.Immutable;
+using VisualHFT.Model;
 
 namespace VisualHFT.Helpers
 {
@@ -7,10 +8,7 @@ namespace VisualHFT.Helpers
     public sealed class HelperOrderBook : IOrderBookHelper
     {
 
-
-        private List<Action<OrderBook>> _subscribers = new List<Action<OrderBook>>();
-
-        private readonly object _lockObj = new object();
+        private ImmutableArray<Action<OrderBook>> _subscribers = ImmutableArray<Action<OrderBook>>.Empty;
 
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -40,39 +38,35 @@ namespace VisualHFT.Helpers
         /// <param name="subscriber">The subscriber.</param>
         public void Subscribe(Action<OrderBook> subscriber)
         {
-            lock (_lockObj)
-            {
-                _subscribers.Add(subscriber);
-            }
+            // No lock needed - ImmutableInterlocked is thread-safe
+            ImmutableInterlocked.Update(ref _subscribers, subs => subs.Add(subscriber));
         }
-
         public void Unsubscribe(Action<OrderBook> subscriber)
         {
-            lock (_lockObj)
-            {
-                _subscribers.Remove(subscriber);
-            }
+            ImmutableInterlocked.Update(ref _subscribers, subs => subs.Remove(subscriber));
+        }
+        public void Reset()
+        {
+            ImmutableInterlocked.InterlockedExchange(ref _subscribers, ImmutableArray<Action<OrderBook>>.Empty);
         }
 
         private void DispatchToSubscribers(OrderBook book)
         {
-            lock (_lockObj)
+            var subscribers = _subscribers;
+            foreach (var subscriber in subscribers)
             {
-                foreach (var subscriber in _subscribers)
+                try
                 {
-                    try
+                    subscriber(book);
+                }
+                catch (Exception ex)
+                {
+                    Task.Run(() =>
                     {
-                        subscriber(book);
-                    }
-                    catch (Exception ex)
-                    {
-                        Task.Run(() =>
-                        {
-                            log.Error(ex);
-                            OnException?.Invoke(new VisualHFT.Commons.Model.ErrorEventArgs(ex, subscriber.Target));
-                        });
-                        throw;
-                    }
+                        log.Error(ex);
+                        OnException?.Invoke(new VisualHFT.Commons.Model.ErrorEventArgs(ex, subscriber.Target));
+                    });
+                    throw;
                 }
             }
         }
@@ -85,14 +79,6 @@ namespace VisualHFT.Helpers
             foreach (var e in data)
             {
                 DispatchToSubscribers(e);
-            }
-        }
-
-        public void Reset()
-        {
-            lock (_lockObj)
-            {
-                _subscribers.Clear();
             }
         }
     }
